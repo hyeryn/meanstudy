0eea54942873c0be527eef44c30354789d764320
hyeryn <hyeryn2000@naver.com>
2021-07-11 14:32:50 +0900

    mongodb connect

--- lecture/index.js
+++ lecture/index.js
@@ -2,13 +2,33 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+const {User} = require('./models/User');
+
+app.use(bodyParser.urlencoded({urlencoded: true}));
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://hyeryn2000:hyeryn2000@cluster0.z46kv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongo_URI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: true
 }).then(()=>console.log('MongoDB connected..'))
 .catch(err=>console.log(err))
 
 app.get('/', (req, res)=>{
-    res.send('hello')
+    res.send('hello 헤링이야')
 })
-app.listen(port, () => console.log("example "+port))
+
+app.post('/register',(req,res)=>{
+    // client에서 보내는 회원가입 정보들을 받아와서 데이터베이스에 저장
+    const user = new User(req.body)
+
+    user.save((err, userInfo)=>{
+        if (err) return res.json({success: false, err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+})
+app.listen(port, () => console.log(`example ${port}`))
