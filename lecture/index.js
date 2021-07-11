09c6a15e987e9998c72718df7735184a7ed2fc71
hyeryn <hyeryn2000@naver.com>
2021-07-11 17:00:12 +0900

    login token 생성

--- lecture/index.js
+++ lecture/index.js
@@ -3,12 +3,14 @@ const app = express()
 const port = 5000
 
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const config = require('./config/key');
 const {User} = require('./models/User');
 
 app.use(bodyParser.urlencoded({urlencoded: true}));
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongo_URI,{
@@ -31,4 +33,32 @@ app.post('/register',(req,res)=>{
         })
     })
 })
+
+app.post('/login', (req, res)=>{
+    // 요청된 이메일을 데이터베이스에 있는지 찾음
+    User.findOne({email: req.body.email}, (err, user)=>{
+        if(!user){
+            return res.json({
+                loginSucess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다"
+            })
+        }
+        // 이메일과 비밀번호가 일치하는지 찾음
+        user.comparePassword(req.body.password, (err, isMatch)=>{
+            if(!isMatch)
+                return res.json({loginSucess: false, message: "비밀번호가 틀렸습니다"})
+
+            // 비밀번호가 같다면 토큰 생성
+            user.generateToken((err, user)=>{
+                if(err) return res.status(400).send(err);
+                // 토큰을 저장한다. -> cookie
+                // console.log("성공")
+                res.cookie("x_auth",user.token )
+                .status(200)
+                .json({loginSuccess:true, userId: user._id})
+            })
+        })
+    })
+})
+
 app.listen(port, () => console.log(`example ${port}`))
